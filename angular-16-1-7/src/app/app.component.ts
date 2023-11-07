c36330c0051918338cc0eea53bf1f0dab55c53c7
sadik.hasan <sadikk.hasan@gmail.com>
2023-11-07 18:30:54 +0600

    new angular project started

---
+++ angular-16-1-7/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import cat from './cat';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-16-1-7';
+  public cat:cat ;
+
+
+
+}
